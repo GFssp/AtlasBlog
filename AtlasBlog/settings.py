f3762f7d442ab16c9769065cb9839c55434fce96
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-03-05 18:22:51 -0300

    settings updated

--- AtlasBlog/settings.py
+++ AtlasBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(0wkl+64qi_+sh4&@1&u8ta#tch3u(-6ig4dxw%u7%vo*&y6xo'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
