441ebbb1fffd7fe2cc5e3403917ecd60435992ff
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-03-05 19:07:20 -0300

    post model updated

--- post/models.py
+++ post/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=60)
-    subtitle = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=60, blank=False)
+    subtitle = models.CharField(max_length=200, blank=False)
+    text = models.TextField(blank=False)
+    pub_date = models.DateField(null=True, blank=False)
+
+    def __str__(self):
+        return f"{self.title}: {self.subtitle}"
     
