6f99841367356a1431bfa223e6960d210566bae0
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-03-07 19:40:34 -0300

    Templates added

--- post/views.py
+++ post/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # This project uses Functions Based View
-def homepage_view(*args, **kwargs):
-    return HttpResponse("<h1>Welcome!</h1>")
+def homepage_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
