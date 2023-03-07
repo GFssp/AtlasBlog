d7ec6088186788a9c7fbf65f30ec9e771229883e
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-03-07 20:36:15 -0300

    about_view added

--- post/views.py
+++ post/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # This project uses Functions Based View
 def homepage_view(request, *args, **kwargs):
     return render(request, "home.html", {})
+
+def about_view(request, *args, **kwargs):
+    context = {
+        "my_text": "This is about us",
+        "code": 202,
+        "my_list": [254, 234]
+    }
+    return render(request, "about.html", context)
