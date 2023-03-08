8c905430f241dca0d5a6d8bda71806628ba02bb2
Guilherme Sampaio <guilhermesampaio@galop.io>
2023-03-08 19:51:26 -0300

    form url added

--- AtlasBlog/urls.py
+++ AtlasBlog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from post.views import homepage_view, about_view, post_detail_view
+from post.views import homepage_view, about_view, post_detail_view, post_create_view
 
 urlpatterns = [
     path('home/', homepage_view, name='home'),
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
-    path('post/', post_detail_view, name='post')
+    path('post/', post_detail_view, name='post'),
+    path('create/', post_create_view, name='create'),
 ]
