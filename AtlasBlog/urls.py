e129c18080a887640f1319feede2bd2cb9b8687d
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-03-07 21:04:57 -0300

    Post url added

--- AtlasBlog/urls.py
+++ AtlasBlog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from post.views import homepage_view, about_view
+from post.views import homepage_view, about_view, post_detail_view
 
 urlpatterns = [
     path('home/', homepage_view, name='home'),
     path('admin/', admin.site.urls),
     path('about/', about_view, name='about'),
+    path('post/', post_detail_view, name='post')
 ]
