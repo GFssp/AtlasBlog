2881d7844802d6eb5b652aa50ebbbcef6e4cd718
Guilherme Sampaio <guilhermesampaio@galop.io>
2023-03-08 19:51:56 -0300

    post create form

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
 
 # This project uses Functions Based View
 def homepage_view(request, *args, **kwargs):
@@ -21,4 +22,17 @@ def post_detail_view(request):
         'subtitle': obj.subtitle,
         'text': obj.text,
     }
+
     return render(request, "posts.html", context)
+
+def post_create_view(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    obj = Post.objects.get(id=1)
+    context = {
+        "form": form
+    }
+
+    return render(request, "post_create.html", context)
